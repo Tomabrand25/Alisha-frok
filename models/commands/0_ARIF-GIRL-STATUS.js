64e9c40258de03f5ec009c2e1886b09385481b89
pagli249 <paglip249@gmail.com>
2025-01-05 12:51:00 +0500

    Update 0_SHAAN-GIRL-STATUS.js

--- models/commands/0_ARIF-GIRL-STATUS.js
+++ models/commands/0_ARIF-GIRL-STATUS.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["♥️SHAAN-KHAN♥️"];
+   var hi = ["♥️𝗔𝗟𝗜𝗦𝗛𝗔-𝗥𝗔𝗡𝗜♥️"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
