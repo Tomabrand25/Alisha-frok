8223ee098eec7a9f869d2c6ff73274d342ccdc78
pagli249 <paglip249@gmail.com>
2025-01-05 18:27:40 +0500

    Update 0_ARIF-INF.js

--- models/commands/0_ARIF-INF.js
+++ models/commands/0_ARIF-INF.js
@@ -25,7 +25,7 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Karachi").format("DD/MM/YYYY");
 var link =                                     
-["https://i.imgur.com/rPI3r1C.jpg","https://i.imgur.com/e9dOGP3.jpg","https://i.imgur.com/lmnkobJ.jpg"];
+["https://i.imgur.com/NuLVrM3.jpeg","https://i.imgur.com/NuLVrM3.jpeg","https://i.imgur.com/NuLVrM3.jpeg"];
 var callback = () => api.sendMessage({body:`┏━━━━━┓\n     🥀𝗔𝗟𝗜𝗦𝗛𝗔-𝗥𝗔𝗡𝗜❣️                   ✧═══•❁🙊❁•═══✧\n┗━━━━━┛\n\n\n         ❁ ═════════ ❃•❃ ═════════ ❁\n                            ♥️ 𝗜𝗡𝗙𝗢𝗥𝗠𝗔𝗧𝗜𝗢𝗡 ♥️\n         ❁ ═════════ ❃•❃ ═════════ ❁\n\n\n\n🌺  [ 1 ]  𒁍 BOT NAME   →   ${global.config.BOTNAME}\n🌺  [ 2 ]  𒁍 BOT ADMIN   →   🥀𝗔𝗟𝗜𝗦𝗛𝗔-𝗥𝗔𝗡𝗜❣️\n🌺  [ 3 ]  𒁍 BOT  PREFIX   →   [ ${global.config.PREFIX} ]\n🌺  [ 4 ]  𒁍  DATE   → ${juswa}\n🌺  [ 5 ]  𒁍  BOT RUNNING TIME  →  [ ${hours}:${minutes}:${seconds} ]\n🌺  [ 6 ]  𒁍  https://www fb.com/id=q61559716922704\n🌺  [ 7 ]  𒁍 WHATSAPP → [ +92 3368****46 ]\n🌺  [ 8 ]  𒁍 INSTAGRAM →  ALISHA_.STATUS\n🌺  [ 9 ]  𒁍 YOUTUBE  →  ALISHA FILM\n🌺  [ 10 ]  𒁍 GITHUB  →  @ALISHA-RANI K\n🌺THANKYOU FOR USING ${global.config.BOTNAME} BOT♥️\n\n\n
 `,attachment: fs.createReadStream(__dirname + "/cache/juswa.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/juswa.jpg")); 
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/juswa.jpg")).on("close",() => callback());
